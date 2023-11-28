07dc863aed5e9010b352c73190bc72ca2d5907f1
Oana-Andreea Ilie <andreeaa.ilie516@gmail.com>
2023-11-28 17:17:37 +0200

    CodaPizza Initial Commit

--- CodaPizza/app/src/main/java/com/android/codapizza/MainActivity.kt
+++ CodaPizza/app/src/main/java/com/android/codapizza/MainActivity.kt
@@ -1,15 +1,53 @@
 package com.android.codapizza
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text(text = "Hello World!")
+            MaterialTheme {
+                Scaffold(
+                    topBar = { MyAppBar() },
+                    content = { padding ->
+                        Text(
+                            text = "Hello World!",
+                            modifier = Modifier.padding(padding)
+                        )
+                    }
+                )
+            }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyAppBar() {
+    TopAppBar(
+        title = {
+            Text(
+                stringResource(id = R.string.app_name),
+                color = Color.White
+            )
+        },
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = Color(0xFF6200EE)
+        )
+    )
+
 }
